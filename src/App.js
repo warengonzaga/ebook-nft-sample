2e417fd4d0444f51b80de480e8c77196f7f90211
Waren Gonzaga <github@warengonzaga.com>
2023-04-27 18:42:40 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import HTMLFlipBook from "react-pageflip";
 import "./styles/Home.css";
 
 export default function Home() {
   return (
     <div className="container">
       <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        <HTMLFlipBook width={300} height={500} showCover={true}>
+          <div className="demoPage">
+            <img alt="demoImage" src="/page1.jpg"/>
+          </div>
+          <div className="demoPage">
+            <img alt="demoImage" src="/page2.jpg"/>
+          </div>
+          <div className="demoPage">
+            <img alt="demoImage" src="/page3.jpg"/>
+          </div>
+          <div className="demoPage">
+            <img alt="demoImage" src="/page4.jpg"/>
+          </div>
+          <div className="demoPage">
+            <img alt="demoImage" src="/page5.jpg"/>
+          </div>
+          <div className="demoPage">
+            <img alt="demoImage" src="/page6.jpg"/>
+          </div>
+        </HTMLFlipBook>
       </main>
     </div>
   );
